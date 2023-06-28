c973fad03b61bd365f87f460d9ffb5efeb13b02b
Louis DEVIE <louisdevie.contact@gmail.com>
2023-06-28 23:28:45 +0200

    Update error.js

--- assets/scripts/error.js
+++ assets/scripts/error.js
@@ -1,5 +1,5 @@
 window.onload = async function () {
-	elizalina.addLanguage("assets/localisation/error_en.json", ["en-GB", "en"]);
-	elizalina.addLanguage("assets/localisation/error_fr.json", ["fr-FR", "fr"]);
+	elizalina.addLanguage("/assets/localisation/error_en.json", ["en-GB", "en"]);
+	elizalina.addLanguage("/assets/localisation/error_fr.json", ["fr-FR", "fr"]);
 	elizalina.fillDocument();
 }
